aaef30f32483bb440ae14fc522fe308820f6a272
Jenni Granados <jennygrana25@gmail.com>
2022-03-23 14:01:25 -0700

    moving folder

--- docs/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import './Header.css';
-
-export default function Header (props) {
-    return (
-        <header className='header'>
-            <h1>{props.title}</h1>
-        </header>
-    )
-}
